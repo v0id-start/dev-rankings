0623808b8c80793430bfe6c67b490f467af15ca7
ehscs-dev <73728431+ehscs-dev@users.noreply.github.com>
2023-08-25 23:54:22 -0700

    Made NavBar, Dropdown value state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import RankingBoard from './components/RankingBoard';
 
 function MyButton() {
   return (
@@ -12,21 +13,7 @@ function MyButton() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MyButton />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RankingBoard />
     </div>
   );
 }
