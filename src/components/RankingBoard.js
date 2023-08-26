156095674172520be6bf3fab9d7a7fa85e685ab0
ehscs-dev <73728431+ehscs-dev@users.noreply.github.com>
2023-08-26 01:36:02 -0700

    Initialize Firebase App, gets image URL from bucket!

--- src/components/RankingBoard.js
+++ src/components/RankingBoard.js
@@ -1,7 +1,29 @@
-
 import React from 'react';
 import { useState } from 'react';
 import NavbarComponent from './NavbarComponent';
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, getDownloadURL} from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnySXXN8iwuJ6pxzuPUfkYIvktqgF8uNI",
+  authDomain: "dev-rankings-397001.firebaseapp.com",
+  projectId: "dev-rankings-397001",
+  storageBucket: "dev-rankings-397001.appspot.com",
+  messagingSenderId: "772219305853",
+  appId: "1:772219305853:web:54d711f4a1748c94605614",
+  measurementId: "G-C5WPPLYT6S"
+};
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage();
+
+// Create a storage reference from our storage service
+const memeRef = ref(storage, 'images/meme.png');
+const memeURL = await getDownloadURL(memeRef);
+
+//const studentFile = ref(storage, 'student_data_small.csv');
 
 function App() {
     const [selectedPeriod, selectPeriod] = useState("All");
@@ -14,7 +36,11 @@ function App() {
   return (
     <div>
       <NavbarComponent onSelect={handleSelectPeriod} />
-      <body><h1>{selectedPeriod}</h1></body>
+
+      <h1>{selectedPeriod}</h1>
+      <img src={memeURL} alt="React meme haha" />
+
+      
       {/* Rest of your app content */}
     </div>
   );
